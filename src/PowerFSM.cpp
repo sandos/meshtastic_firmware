be73ed684141a06a170df78c68f8d7d019e1505a
John Bäckstrand <john.backstrand1@hitachienergy.com>
2025-11-22 01:22:27 +0100

    moar

--- src/PowerFSM.cpp
+++ src/PowerFSM.cpp
@@ -280,6 +280,9 @@ void PowerFSM_setup()
     bool hasPower = isPowered();
 
     LOG_INFO("PowerFSM init, USB power=%d", hasPower ? 1 : 0);
+    LOG_INFO("PowerFSM config: screen_on_secs=%u power_saving=%d min_wake_secs=%u wait_bluetooth_secs=%u",
+             config.display.screen_on_secs, config.power.is_power_saving, config.power.min_wake_secs,
+             config.power.wait_bluetooth_secs);
     powerFSM.add_timed_transition(&stateBOOT, hasPower ? &statePOWER : &stateON, 3 * 1000, NULL, "boot timeout");
 
     // wake timer expired or a packet arrived
